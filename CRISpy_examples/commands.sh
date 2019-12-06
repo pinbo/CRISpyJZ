8f720c617adfa041a8a8c1276e0fb51e54ffc57e
Junli Zhang <zhjl86@gmail.com>
2019-12-05 21:44:37 -0800

    rerun the examples with new script v2

--- CRISpy_examples/commands.sh
+++ CRISpy_examples/commands.sh
@@ -1,5 +1,5 @@
-python2 ../CRISpy_v1_JZ.py ../reference-example.fasta gene1-g10 gene1 + gcggagaactg ccggcccgga GAGGCAGGCGTCGAAGAGTACGG 
+python ../CRISpy_JZ_v2.py ../reference-example.fasta gene1-g10 gene1 + gcggagaactg ccggcccgga GAGGCAGGCGTCGAAGAGTACGG 
 
-python2 ../CRISpy_v1_JZ.py ../reference-example.fasta gene1-g14 gene1 + gcggagaactg ccggcccgga CGGCCCTGAAGAAGACGGCGGGG
+python ../CRISpy_JZ_v2.py ../reference-example.fasta gene1-g14 gene1 + gcggagaactg ccggcccgga CGGCCCTGAAGAAGACGGCGGGG
 
-python2 ../CRISpy_v1_JZ.py ../reference-example.fasta gene2-g6 gene2 + TCCTTTGAACAAA AGGCTTTTGTTCAG CCTGCGCCACTACTACAAACTAA
+python ../CRISpy_JZ_v2.py ../reference-example.fasta gene2-g6 gene2 + TCCTTTGAACAAA AGGCTTTTGTTCAG CCTGCGCCACTACTACAAACTAA
